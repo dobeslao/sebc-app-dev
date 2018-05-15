2d85febd56adde29d93e8183816387a2e67a9b21
August Dobeslao <dobeslao@vinkos.com>
2018-05-15 18:56:50 -0500

    Spark run on cluster mode with yarn at 128 MB (code and build environment)

--- spark/src/main/scala/com/cloudera/sdk/SparkBatchExample.scala
+++ spark/src/main/scala/com/cloudera/sdk/SparkBatchExample.scala
@@ -15,13 +15,25 @@ object SparkBatchExample {
         val path = args(0)
         val conf = new SparkConf()
             .setAppName("Spark Batch Example")
-            .setMaster("local[2]")
+            // .setMaster("local[2]")
             .set("spark.io.compression.codec", "lz4")
         val sc = new SparkContext(conf)
  
         conf.set("spark.cleaner.ttl", "120000")
+        
+        /* CLEAN HDFS OUTPUT FOLDER */
+        val hdfs = FileSystem.get(sc.hadoopConfiguration)
+        val basePath = new Path(path + "/output/")
+        hdfs.delete(basePath, true)
 
         /* READ DATA FROM HDFS, SORT BY KEY, SAVE TO HDFS */
+        val file = sc.textFile(path + "/data/file*.csv")
+        val tuple = file.map(line => line.split(","))
+                 .map(array => (array(0), array(1)))
+        val reducer = tuple.coalesce(1, true)
+        val sorted = reducer.sortByKey()
+        
+        // Save sorted results
+        sorted.saveAsTextFile(path + "/output/")
     }
 }
-
